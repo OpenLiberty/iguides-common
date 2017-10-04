77b9fcedd9c9e7eaee8d74089bf899b6805c69bb
Denise Morganti <dmorgant@us.ibm.com>
2017-10-04 15:25:09 -0500

    Update Reload on web browser to Refresh

--- js/interactive-guides/messages.js
+++ js/interactive-guides/messages.js
@@ -5,7 +5,7 @@ var messages = {
   "browserSample": "Sample browser window",
   "browserAddressBar": "Sample browser address bar",
   "browserContentIdentifier": "Sample browser display window",
-  "browserRefreshButton": "Reload sample browser page",
+  "browserRefreshButton": "Refresh sample browser page",
   "navigationButtons": "Guide navigation buttons",
   "prevButton": "Previous",
   "nextButton": "Next",
