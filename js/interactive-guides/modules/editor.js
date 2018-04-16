3f61590e8d4da9f761a7676a194a3862ba42d285
Annie Nguyen <aknguyen@us.ibm.com>
2018-04-16 14:16:38 -0500

    set editor height in json when editor is created

--- js/interactive-guides/modules/editor.js
+++ js/interactive-guides/modules/editor.js
@@ -151,12 +151,18 @@ var editor = (function() {
                     if (content.fileName) {
                         container.find('.editorContainer').attr("aria-label", content.fileName + " editor");
                         container.find('.editorFileName').text(content.fileName);
-                        thisEditor.fileName = content.fileName;
+                        thisEditor.fileName = content.fileName;              
                         //$(".editorContainer").css("margin-top", "-20px");
                         container.find(".editorContainer").css({
                             "margin-top": "-20px",
-                            "margin-bottom": "50px"
+                            "margin-bottom": "50px",
+                            "height": content.editorHeight
                         });
+                        if (content.editorHeight !== undefined) {                       
+                            container.find(".editorContainer").css({
+                                "height": content.editorHeight
+                            });
+                        }
                     }
                     var editor = container.find('.codeeditor');
                     //console.log("container id", container[0].id);
